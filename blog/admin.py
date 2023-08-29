208b48e2052ea2d822aeb4d013001acd58452ef3
Tomi <somitamas1995@gmail.com>
2023-08-29 12:11:11 +0000

    add comment panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+# it will register both post model and the post admin class with our admin site
+@admin.register(Post) 
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
